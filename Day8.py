6156778316c6d6c0b1b94059e9203a3bbc914010
Anjali-Kumbhar <159118771+Anjali-Kumbhar@users.noreply.github.com>
2025-08-23 00:06:57 +0530

    Day8.py

--- Day8.py
+++ Day8.py
@@ -1,38 +1,10 @@
-def reverseW(s: str) -> str:
-    def cleanSpaces(chars):
-        n = len(chars)
-        i, j = 0, 0
-        while j < n:
-            while j < n and chars[j] == " ":
-                j += 1
-            while j < n and chars[j] != " ":
-                chars[i] = chars[j]
-                i += 1
-                j += 1
-            while j < n and chars[j] == " ":
-                j += 1
-            if j < n:
-                chars[i] = " "
-                i += 1
-        return chars[:i]
-    def reverse(chars, left, right):
-        while left < right:
-            chars[left], chars[right] = chars[right], chars[left]
-            left += 1
-            right -= 1
-    chars = list(s)
-    chars = charsSpaces(chars)
-    reverse(chars, 0, len(chars) - 1)
-    n = len(chars)
-    start = 0
-    for end in range(n + 1):
-        if end == n or chars[end] == " ":
-            reverse(chars, start, end - 1)
-            start = end + 1
 
-    return "".join(chars)
+def reverse(s: str) -> str:
+    words = s.split()               
+    reversed_words = [w[::-1] for w in words]  
+    return " ".join(reversed_words)
 
 
 if __name__ == "__main__":
-    s = "   hello   world   from   Java   "
-    print(reverseW(s))
+    s = "Java from world hello"
+    print(reverse(s))
